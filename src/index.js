c0f5262448e1baadcc96a36fdfe00b5255403ad7
Tom Henderson <tomhenderson@mac.com>
2022-06-02 09:57:40 +1200

    Listen on all paths

--- src/index.js
+++ src/index.js
@@ -6,24 +6,24 @@ var hostname = os.hostname();
 
 app.use(express.static('public'));
 
-app.get('/', function (req, res) {
-  res.send('<html> \
-    <head> \
-        <title>Docker</title> \
-        <link rel="stylesheet" type="text/css" href="css/style.css"> \
-    </head> \
-    <body> \
-        <h1>Hello from ' + hostname + '</h1> \
-    <body/> \
-</html>');
-});
-
 app.get('/health', function (req, res) {
       res.format({
       'text/plain': function(){
         res.send('ok');
       }
     });
+});
+
+app.get('/*', function (req, res) {
+  res.send('<html> \
+    <head> \
+        <title>Docker</title> \
+        <link rel="stylesheet" type="text/css" href="/css/style.css"> \
+    </head> \
+    <body> \
+        <h1>Hello from ' + hostname + req.path + '</h1> \
+    <body/> \
+</html>');
 });
 
 var port = process.env.PORT || 3000;
